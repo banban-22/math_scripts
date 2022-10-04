036a3067219a0e6e8f4ebf18f9efc20ebd92180a
Hooman.Minoo <hoomanminoosepehr@gmail.com>
2022-10-04 15:54:29 -0700

    my way to sum numbers

---
+++ multyply2.js
@@ -0,0 +1,6 @@
+// this is my way to add two numbber
+let num1 = parseInt(prompt("please enter the first number"))
+let num2 = parseInt(prompt("please enter the seconf number"))
+let num3 = num1 + num2;
+alert (`sum ${num1} & ${num2} is equal to ${num3}`)
+
